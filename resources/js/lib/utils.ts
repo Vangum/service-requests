7655998603b3cc8fa2e96884ad528ff401d09bf1
mavrenkov <vasiliymavrenkov@gmail.com>
2026-01-09 13:44:09 +0300

    feat(layout): add layouts

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -1,6 +1,18 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import {InertiaLinkProps} from "@inertiajs/vue3";
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
+}
+
+export function urlIsActive(
+    urlToCheck: NonNullable<InertiaLinkProps['href']>,
+    currentUrl: string,
+) {
+    return toUrl(urlToCheck) === currentUrl;
+}
+
+export function toUrl(href: NonNullable<InertiaLinkProps['href']>) {
+    return typeof href === 'string' ? href : href?.url;
 }
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -2,10 +2,18 @@ export interface Auth {
     user: User;
 }
 
+export interface NavItem {
+    title: string;
+    href: NonNullable<InertiaLinkProps['href']>;
+    icon?: LucideIcon;
+    isActive?: boolean;
+}
+
 export type AppPageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
     name: string;
     quote: { message: string; author: string };
     auth: Auth;
+    sidebarOpen: boolean;
 };
 
 export interface User {
